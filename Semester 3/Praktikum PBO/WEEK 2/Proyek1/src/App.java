0c03ad6f398804fc99632f4686ef6b843a23ffff
RestuWibisonoo <restu.wibisonoo@gmail.com>
2024-08-28 05:35:07 +0700

    tugas

---
+++ Semester 3/Praktikum PBO/WEEK 2/Proyek1/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Nama : Restu Wibisono");
+        System.out.println("NPM : 2340506061");
+        System.out.println("Alamat : Magelang Utara");
+        System.out.println("No HP : +6285727426041");
+    }
+}
