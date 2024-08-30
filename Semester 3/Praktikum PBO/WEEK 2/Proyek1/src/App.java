ae97b4fa57804bf6fd0c708baf4c30d6dd7a648f
RestuWibisonoo <restu.wibisonoo@gmail.com>
2024-08-30 18:32:05 +0700

    update

--- Semester 3/Praktikum PBO/WEEK 2/Proyek1/src/App.java
+++ Semester 3/Praktikum PBO/WEEK 2/Proyek1/src/App.java
@@ -3,6 +3,6 @@ public class App {
         System.out.println("Nama : Restu Wibisono");
         System.out.println("NPM : 2340506061");
         System.out.println("Alamat : Magelang Utara");
-        System.out.println("No HP : +6285727426041");
+        System.out.println("No HP : +62857274260");
     }
 }
