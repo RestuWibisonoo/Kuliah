f75091355c553a5833716e887b7795af641aea7f
RestuWibisonoo <restu.wibisonoo@gmail.com>
2024-10-21 13:52:29 +0700

    tugas pbo

---
+++ Semester 3/Praktikum PBO/WEEK9/Tugas9/src/Main.java
@@ -0,0 +1,27 @@
+abstract class Hewan {
+    abstract void suara();
+}
+
+class Kucing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Kucing mengeluarkan suara: Meong");
+    }
+}
+
+class Anjing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Anjing mengeluarkan suara: Guk Guk");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Hewan kucing = new Kucing();
+        Hewan anjing = new Anjing();
+
+        kucing.suara();
+        anjing.suara();
+    }
+}
