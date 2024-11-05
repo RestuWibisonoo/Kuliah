7d567273488048b85cb4f4d2303d7bf57f6dfff5
RestuWibisonoo <restu.wibisonoo@gmail.com>
2024-11-05 21:25:57 +0700

    tugas

--- Semester 3/Praktikum PBO/WEEK10/Tugas10/src/Main.java
+++ Semester 3/Praktikum PBO/WEEK10/Tugas10/src/Main.java
@@ -1,32 +1,28 @@
-// Interface Pembayaran
 interface Pembayaran {
     double hitungPajak(double harga);
 }
 
-// Kelas Elektronik yang mengimplementasikan Pembayaran
 class Elektronik implements Pembayaran {
     @Override
     public double hitungPajak(double harga) {
-        return harga * 0.10;  // Pajak 10%
+        return harga * 0.10;
     }
 }
 
-// Kelas Makanan yang mengimplementasikan Pembayaran
 class Makanan implements Pembayaran {
     @Override
     public double hitungPajak(double harga) {
-        return harga * 0.05;  // Pajak 5%
+        return harga * 0.05;
     }
 }
 
-// Kelas Main untuk menjalankan program
 public class Main {
     public static void main(String[] args) {
         Pembayaran elektronik = new Elektronik();
         Pembayaran makanan = new Makanan();
 
-        double hargaElektronik = 1000000;  // Contoh harga
-        double hargaMakanan = 50000;       // Contoh harga
+        double hargaElektronik = 1000000;
+        double hargaMakanan = 50000;
 
         System.out.println("Pajak Elektronik: " + elektronik.hitungPajak(hargaElektronik));
         System.out.println("Pajak Makanan: " + makanan.hitungPajak(hargaMakanan));
